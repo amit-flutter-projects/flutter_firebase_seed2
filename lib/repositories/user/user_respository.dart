ef05c5dea901346051997e8b10537a38773384b4
gargrishika5@gmail.com <gargrishika5@gmail.com>
2021-05-16 14:47:32 +0530

    user repository

---
+++ lib/repositories/user/user_respository.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../config/Paths.dart';
+import '../../models/user_model.dart';
+import '../../repositories/user/base_user_repository.dart';
+import 'package:meta/meta.dart';
+
+class UserRepository extends BaseUserRepository {
+  FirebaseFirestore _firebaseFirestore;
+
+  UserRepository({
+    @required FirebaseFirestore firebaseFirestore,
+  }) : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+
+  @override
+  Future<UserModel> getUserWithId({@required String userId}) async {
+    final doc = await _firebaseFirestore
+        .collection(Paths.users)
+        .doc(userId)
+        .get() as DocumentSnapshot<UserModel>;
+
+    return doc.exists ? UserModel.fromDocument(doc) : null;
+    // return doc.exists ? UserModel.fromDocument(doc) : UserModel.empty;
+  }
+
+  @override
+  Future<void> updateUser({@required UserModel user}) async {
+    await _firebaseFirestore
+        .collection(Paths.users)
+        .doc(user.id)
+        .update(user.toDocument());
+  }
+
+  Future<List<UserModel>> getAllUsers() async {
+    QuerySnapshot<UserModel> querySnapshot = await _firebaseFirestore
+        .collection(Paths.users)
+        .get() as QuerySnapshot<UserModel>;
+    return UserModel.getUserListFromQuerySnapshot(querySnapshot);
+  }
+
+  Future<void> deleteUser(String userId) async {
+    await _firebaseFirestore.collection(Paths.users).doc(userId).delete();
+  }
+}
